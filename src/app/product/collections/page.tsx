20175cb66d331b4171c32d64145fa504769c2f46
iulic876 <botnaruiulian388@gmail.com>
2025-06-21 22:16:11 +0300

    vercel fixes 3

--- src/app/product/collections/page.tsx
+++ src/app/product/collections/page.tsx
@@ -1,7 +1,7 @@
 import { TabsProvider } from "@/components/tabs/TabsContext";
 import { TabsBar } from "@/components/tabs/TabsBar";
 import { TabContent } from "@/components/tabs/TabContent";
-import { TabOpener } from "@/components/tabs/TabOpener";
+
 
 export default function Page() {
   return (
