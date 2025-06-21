a4178e7b5cd1414340ff31d5a439ecce1d72d157
iulic876 <botnaruiulian388@gmail.com>
2025-06-21 10:46:12 +0300

    final fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+'use client'
 import React from 'react'
+import { useRouter } from 'next/navigation'
 
 const page = () => {
+
+  const router = useRouter()
   return (
-    <div>
-      test
+    <div className='flex flex-col'>
+      <button onClick={() => router.push("/login")}>test</button>
+      <button onClick={() => router.push("/product/collections")}>test</button>
     </div>
-  )
+  );
 }
 
 export default page
